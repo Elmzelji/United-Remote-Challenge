aabe037784823affd8d6868b2e56a4b0ddbf845e
ELMZELJI Youssef <youssefelmzelji@gmail.com>
2019-06-29 16:55:23 +0000

    Added Like and Dislike Functionality with testing auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
     db.query('SELECT id, username, email FROM users WHERE id = ?', [req.session.user], function(error, results, fields) {
 			if (results.length > 0) {
         
-        db.query('SELECT * FROM shops', function(error, shops, fields) {
+        db.query('SELECT s.`*` FROM shops s LEFT JOIN shop_status ss ON s.id = ss.shop WHERE ss.status IS NULL OR ss.status = 0 UNION SELECT s.* FROM shops s RIGHT JOIN shop_status ss ON s.id = ss.shop WHERE ss.status IS NULL OR ss.status = 0', function(error, shops, fields) {
           res.render('index', { title: 'Shop Directory', user: {id: results[0].id, username: results[0].username, email: results[0].email}, shops:shops });
         })
 
@@ -21,6 +21,24 @@ router.get('/', function(req, res, next) {
     db.query('SELECT * FROM shops', function(error, shops, fields) {
       res.render('index', { title: 'Shop Directory', shops: shops });
     })
+  }
+});
+
+/* Get Pref Page */
+router.get('/pref', (req, res, next) => {
+  if(req.session.user){
+
+    db.query('SELECT id, username, email FROM users WHERE id = ?', [req.session.user], function(error, results, fields) {
+      if (results.length > 0) {
+        db.query("SELECT s.id AS 'store_id', s.*, ss.`*` FROM shops s JOIN shop_status ss ON s.id = ss.shop JOIN users u ON u.id = ss.user WHERE ss.`status` = 1 AND u.id = ?", [req.session.user], function(error, shops, fields) {
+          res.render('pref', { title: 'Prefered Shops', user: {id: results[0].id, username: results[0].username, email: results[0].email}, shops:shops });
+        })
+      }
+    });
+
+    
+  }else{
+    res.redirect('/auth/login');
   }
 });
 
--- src/routes/shops.js
+++ src/routes/shops.js
@@ -3,11 +3,47 @@ var router = express.Router();
 const db = require('../db.js');
 
 router.get('/like/:id', (req, res, next) => {
-    res.send("Liked");
+    if(req.session.user){
+        var store_id = req.params.id;
+        var user_id = req.session.user;
+        var liked = 1;
+
+        db.query('SELECT * FROM shops WHERE id = ?', [store_id], function(error, results, fields) {
+            if (results.length > 0) {
+                db.query('INSERT INTO shop_status (shop, user, status) VALUES(?, ?, ?)', [store_id, user_id, liked], (err, results, fields) => {
+                    if(err) throw err;
+                    res.redirect('/pref');
+                });
+            }else{
+                res.redirect('/');
+            }
+        });
+
+    }else{
+        res.redirect('/auth/login');
+    }
 });
 
 router.get('/dislike/:id', (req, res, next) => {
-    res.send("Disliked");
+    if(req.session.user){
+        var store_id = req.params.id;
+        var user_id = req.session.user;
+        var liked = 0;
+
+        db.query('SELECT * FROM shops WHERE id = ?', [store_id], function(error, results, fields) {
+            if (results.length > 0) {
+                db.query('INSERT INTO shop_status (shop, user, status) VALUES(?, ?, ?)', [store_id, user_id, liked], (err, results, fields) => {
+                    if(err) throw err;
+                    res.redirect('/pref');
+                });
+            }else{
+                res.redirect('/');
+            }
+        });
+
+    }else{
+        res.redirect('/auth/login');
+    }
 });
 
 module.exports = router;
