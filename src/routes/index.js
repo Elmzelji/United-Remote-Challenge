f52335ace28233c1126f5f019e82291b26259213
ELMZELJI Youssef <youssefelmzelji@gmail.com>
2019-06-30 15:14:47 +0000

    Fixed the remove from prefered shops bugs, added dislike functionality 2h

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 const db = require('../db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log(req.session.user);
   if(req.session.user){
 
     db.query('SELECT id, username, email FROM users WHERE id = ?', [req.session.user], function(error, results, fields) {
 			if (results.length > 0) {
         
-        db.query('SELECT s.`*` FROM shops s LEFT JOIN shop_status ss ON s.id = ss.shop WHERE ss.status IS NULL OR ss.status = 0 UNION SELECT s.* FROM shops s RIGHT JOIN shop_status ss ON s.id = ss.shop WHERE ss.status IS NULL OR ss.status = 0', function(error, shops, fields) {
-          res.render('index', { title: 'Shop Directory', user: {id: results[0].id, username: results[0].username, email: results[0].email}, shops:shops });
+        db.query('SELECT s.*, d.disliked_at, d.removed_at FROM shops s LEFT JOIN dislike d ON s.id=d.store LEFT JOIN shop_status ss ON s.id = ss.shop WHERE ss.`status` IS NULL OR ss.`status` = 0', function(error, shops, fields) {
+          var now = moment().format("YYYY-MM-DD HH:mm:ss");
+          var nearby = [];
+          
+          shops.forEach(shop => {
+            if(shop.removed_at != null){
+              var removed_date = moment(shop.removed_at);
+              if(now > removed_date){
+                nearby.push(shop);
+              }
+            }else{
+              nearby.push(shop);
+            }
+
+          });
+
+          res.render('index', { title: 'Shop Directory', user: {id: results[0].id, username: results[0].username, email: results[0].email}, shops:nearby, date:now });
         })
 
 			}		
--- src/routes/shops.js
+++ src/routes/shops.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 const db = require('../db.js');
 
 router.get('/like/:id', (req, res, next) => {
@@ -10,10 +11,24 @@ router.get('/like/:id', (req, res, next) => {
 
         db.query('SELECT * FROM shops WHERE id = ?', [store_id], function(error, results, fields) {
             if (results.length > 0) {
-                db.query('INSERT INTO shop_status (shop, user, status) VALUES(?, ?, ?)', [store_id, user_id, liked], (err, results, fields) => {
-                    if(err) throw err;
-                    res.redirect('/pref');
-                });
+                db.query('SELECT * FROM shop_status WHERE shop = ?', [store_id], function(error, results, fields) {
+                    if (results.length > 0) {
+                        var status_id = results[0].id;
+                        if(results[0].status == 1){
+                            res.redirect('/');
+                        }else{
+                            db.query('UPDATE shop_status SET status = 1 WHERE id = ?', [status_id], (err, results, fields) => {
+                                if(err) throw err;
+                                res.redirect('/pref');
+                            });
+                        }
+                    } else{
+                        db.query('INSERT INTO shop_status (shop, user, status) VALUES(?, ?, ?)', [store_id, user_id, liked], (err, results, fields) => {
+                            if(err) throw err;
+                            res.redirect('/pref');
+                        });
+                    }
+                })
             }else{
                 res.redirect('/');
             }
@@ -25,10 +40,59 @@ router.get('/like/:id', (req, res, next) => {
 });
 
 router.get('/dislike/:id', (req, res, next) => {
+    //Dislike A shop
+    //Won't Show in nearby shop intil the next 2h
+    //Get Store_ID and User_ID and TimeClicked
     if(req.session.user){
         var store_id = req.params.id;
         var user_id = req.session.user;
-        var liked = 0;
+        
+        //Test if this selected Shop is available
+        db.query('SELECT * FROM shops WHERE id = ?', [store_id], function(error, results, fields) {
+            if (results.length > 0) {
+                //Test if it's already disliked
+                db.query('SELECT * FROM dislike WHERE store = ?', [store_id], function(error, results, fields) {
+                    if (results.length > 0) {
+                        //Already Disliked
+                        var now = moment().format("YYYY-MM-DD HH:mm:ss");
+                        var done_date = moment(results[0].removed_at);
+                        var newdone = moment().add(2, 'hours').format("YYYY-MM-DD HH:mm:ss");
+
+                        if(now > done_date) {
+
+                            db.query('UPDATE dislike SET disliked_at = ? AND removed_at = ? WHERE id = ?', [now, newdone, results[0].id], (err, results, fields) => {
+                                if(err) throw err;
+                                res.redirect('/');
+                            });
+
+                        }
+
+                        res.redirect('/');
+                    }
+                    else{
+                        //Store in Database and return to home
+                        //Get Date in Time
+                        var now = moment().format("YYYY-MM-DD HH:mm:ss");
+                        var done = moment().add(2, 'hours').format("YYYY-MM-DD HH:mm:ss");
+
+                        db.query('INSERT INTO dislike (store, user, disliked_at, removed_at) VALUES(?, ?, ?, ?)', [store_id, user_id, now, done], (err, results, fields) => {
+                            if(err) throw err;
+                            res.redirect('/');
+                        });
+
+                    }
+                })
+            }
+        })
+    }else{
+        res.redirect('/auth/login');
+    }
+});
+
+router.get('/remove/:id', (req, res, next) => {
+    if(req.session.user){
+        var store_id = req.params.id;
+        var user_id = req.session.user;
 
         db.query('SELECT * FROM shops WHERE id = ?', [store_id], function(error, results, fields) {
             if (results.length > 0) {
